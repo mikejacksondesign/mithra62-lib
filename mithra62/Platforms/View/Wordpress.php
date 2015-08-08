666ec51578647d1f46d9d41909b10f458eaccfca
Eric Lamb <eric@mithra62.com>
2015-08-07 21:50:45 -0700

    Adds base Table View Helper Adds base Wordpress View and Platform objects

---
+++ mithra62/Platforms/View/Wordpress.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * mithra62 - Backup Pro
+ *
+ * @copyright	Copyright (c) 2015, mithra62, Eric Lamb.
+ * @link		http://mithra62.com/
+ * @version		3.0
+ * @filesource 	./mithra62/BackupPro/Platforms/View/Eecms.php
+ */
+ 
+namespace mithra62\Platforms\View;
+
+
+/**
+ * Backup Pro - Eecms View Object
+ *
+ * Contains the view helpers for ExpressionEngine
+ *
+ * @package 	BackupPro\View
+ * @author		Eric Lamb <eric@mithra62.com>
+ */
+class Wordpress extends AbstractView
+{
+
+}
---
+++ mithra62/Platforms/Wordpress.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * mithra62
+ *
+ * @copyright	Copyright (c) 2015, mithra62, Eric Lamb.
+ * @link		http://mithra62.com/
+ * @version		3.0
+ * @filesource 	./mithra62/Platforms/Eecms.php
+ */
+ 
+namespace mithra62\Platforms;
+
+use mithra62\AbstractPlatform;
+
+/**
+ * mithra62 - Wordpress Platform Object
+ *
+ * The bridge between mithra62 code and Wordpress specific logic
+ *
+ * @package 	mithra62\Wordpress
+ * @author		Eric Lamb <eric@mithra62.com>
+ */
+class Wordpress extends AbstractPlatform
+{
+    /**
+     * The Eecms config array
+     * @var array
+     */
+    private $config = null;
+    
+    /**
+     * Sets the Eecms config array
+     * @param array $config
+     */
+    public function setConfig( array $config)
+    {
+        $this->config = $config;    
+    }
+    
+    /**
+     * (non-PHPdoc)
+     * @see \mithra62\Platforms::getDbCredentials()
+     */
+    public function getDbCredentials()
+    {
+        global $wpdb;
+        return array(
+            'user' => DB_USER,
+            'password' => DB_PASSWORD,
+            'database' => DB_NAME,
+            'host' => DB_HOST,
+            'prefix' => $wpdb->prefix,
+            'settings_table_name' => $wpdb->prefix.'backup_pro_settings'
+        );
+    }
+    
+    /**
+     * (non-PHPdoc)
+     * @ignore
+     * @see \mithra62\BackupPro\Platforms\PlatformInterface::getEmailDetails()
+     */
+    public function getEmailConfig()
+    {
+        $this->email_config['type'] = ee()->config->config['mail_protocol'];
+        $this->email_config['port'] = ee()->config->config['smtp_port'];
+        if( $this->email_config['type'] == 'smtp' )
+        {
+            $this->email_config['smtp_options']['host'] = ee()->config->config['smtp_server'];
+            $this->email_config['smtp_options']['connection_config']['username'] = ee()->config->config['smtp_username'];
+            $this->email_config['smtp_options']['connection_config']['password'] = ee()->config->config['smtp_password'];
+            $this->email_config['smtp_options']['port'] = $this->email_config['port'];
+        }
+        
+        $this->email_config['sender_name'] = ee()->config->config['site_label'];
+        $this->email_config['from_email'] = ee()->config->config['webmaster_email'];
+        
+        return $this->email_config;
+    }
+    
+    /**
+     * (non-PHPdoc)
+     * @see \mithra62\BackupPro\Platforms\PlatformInterface::getCurrentUrl()
+     */
+    public function getCurrentUrl()
+    {
+        if( !function_exists('current_url') )
+        {
+            ee()->load->helper('url');
+        }
+        
+        return current_url();
+    }
+    
+    /**
+     * (non-PHPdoc)
+     * @see \mithra62\Platforms::getSiteName()
+     */
+    public function getSiteName()
+    {
+        return ee()->config->config['site_name'];
+    }
+    
+    /**
+     * (non-PHPdoc)
+     * @see \mithra62\Platforms::getTimezone()
+     */
+    public function getTimezone() 
+    {
+        
+    }
+    
+    /**
+     * (non-PHPdoc)
+     * @see \mithra62\Platforms::getSiteUrl()
+     */
+    public function getSiteUrl()
+    {
+        if( !function_exists('site_url') )
+        {
+            ee()->load->helper('url');
+        }
+        
+        return site_url();
+    }
+}
