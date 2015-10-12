8a40a5f549e590d7470b4ad7ab9d51f86272169e
Eric Lamb <eric@mithra62.com>
2015-10-12 16:05:53 -0700

    Adds EE3 Platform object

---
+++ mithra62/Platforms/View/Ee3.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * mithra62 - Jaeger
+ *
+ * @copyright	Copyright (c) 2015, mithra62, Eric Lamb.
+ * @link		http://mithra62.com/
+ * @version		3.0
+ * @filesource 	./mithra62/Platforms/View/Eecms.php
+ */
+ 
+namespace mithra62\Platforms\View;
+
+
+/**
+ * Backup Pro - Eecms View Object
+ *
+ * Contains the view helpers for ExpressionEngine
+ *
+ * @package 	View
+ * @author		Eric Lamb <eric@mithra62.com>
+ */
+class Ee3 extends AbstractView
+{
+
+}
