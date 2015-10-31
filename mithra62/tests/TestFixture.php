426bff5d3361c0584f7aeac7f89d19b5d35a9d3d
Eric Lamb <eric@mithra62.com>
2015-10-31 14:36:59 -0700

    Adds Shell and GCS Connect Validation Rule tests

--- mithra62/tests/TestFixture.php
+++ mithra62/tests/TestFixture.php
@@ -20,13 +20,25 @@ namespace mithra62\tests;
  */
 class TestFixture extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * The name of the test database table
+     * @var string 
+     */
     protected $test_table_name = 'm62_test_table';
     
+    /**
+     * The full path to the working directory any file system activity happens
+     * @return string
+     */
     protected function getWorkingDir()
     {
         return rtrim(realpath(dirname(__FILE__).'/working_dir'), DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR;
     }
     
+    /**
+     * The full path to the data directory 
+     * @return string
+     */
     protected function dataPath()
     {
         return realpath(dirname(__FILE__).'/data');
@@ -59,21 +71,42 @@ class TestFixture extends \PHPUnit_Framework_TestCase
         return realpath(dirname(__FILE__).'/data/languages/language3');
     }   
     
+    /**
+     * The Amazon S3 Test Credentials
+     * @return array
+     */
     protected function getS3Creds()
     {
         return include 'data/s3creds.config.php';
     }
     
+    /**
+     * The FTP Test Credentials
+     * @return array
+     */
     protected function getFtpCreds()
     {
         return include 'data/ftpcreds.config.php';
     }
     
+    protected function getGcsCreds()
+    {
+        return include 'data/gcscreds.config.php';
+    }
+    
+    /**
+     * The Databaes Test Credentiasl
+     * @return array
+     */
     protected function getDbCreds()
     {
         return include 'data/db.config.php';
     }
     
+    /**
+     * The SQL string to create the test table
+     * @return string
+     */
     protected function getSettingsTableCreateSql()
     {
         return include 'data/db.sql.php';
