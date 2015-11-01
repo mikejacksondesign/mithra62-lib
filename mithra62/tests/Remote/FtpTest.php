7f4d01b2649c335046c6f8fd5442eafa4e823dba
Eric Lamb <eric@mithra62.com>
2015-10-31 20:07:45 -0700

    Adds Remote test Adds stub for Remote FTP test

---
+++ mithra62/tests/Remote/FtpTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * mithra62 - Unit Test
+ *
+ * @copyright	Copyright (c) 2015, mithra62, Eric Lamb.
+ * @link		http://mithra62.com/
+ * @version		1.0
+ * @filesource 	./mithra62/tests/Remote/FtpTest.php
+ */
+ 
+namespace mithra62\tests\Traits;
+
+use mithra62\tests\TestFixture;
+
+/**
+ * mithra62 - Ftp Remote Object Unit Tests
+ *
+ * Contains all the unit tests for the \mithra62\Remote\Ftp object
+ *
+ * @package 	mithra62\Tests
+ * @author		Eric Lamb <eric@mithra62.com>
+ */
+class FtpTest extends TestFixture
+{
+    public function testInstance()
+    {
+        
+    }
+}
