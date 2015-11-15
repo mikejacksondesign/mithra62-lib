80b39a4b06583b3774bdde9995efe9a3631551b3
Eric Lamb <eric@mithra62.com>
2015-11-14 16:04:06 -0800

    Adds Console Platform

--- mithra62/Console.php
+++ mithra62/Console.php
@@ -1,10 +1,31 @@
 <?php
+/**
+ * mithra62
+ *
+ * @copyright	Copyright (c) 2015, mithra62, Eric Lamb.
+ * @link		http://mithra62.com/
+ * @version		3.0
+ * @filesource 	./mithra62/Console.php
+ */
+ 
 namespace mithra62;
 
 use Commando\Command;
 
+/**
+ * mithra62 - Console Object
+ *
+ * Abstracts our Console interface 
+ *
+ * @package 	Platforms\Console
+ * @author		Eric Lamb <eric@mithra62.com>
+ */
 class Console extends Command
 {
+    /**
+     * The cli input string
+     * @param string $tokens
+     */
     public function __construct($tokens = null)
     {
         parent::__construct($tokens);
---
+++ mithra62/Platforms/Console.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * mithra62
+ *
+ * @copyright	Copyright (c) 2015, mithra62, Eric Lamb.
+ * @link		http://mithra62.com/
+ * @version		3.0
+ * @filesource 	./mithra62/Platforms/Console.php
+ */
+ 
+namespace mithra62\Platforms;
+
+use mithra62\Platforms\AbstractPlatform;
+
+/**
+ * mithra62 - Console Platform Object
+ *
+ * The bridge between mithra62 code and Console specific logic
+ *
+ * @package 	Platforms\Console
+ * @author		Eric Lamb <eric@mithra62.com>
+ */
+class Console extends AbstractPlatform
+{
+    /**
+     * The set settings data from the configuration file
+     * @var array
+     */
+    protected $config = array();
+    
+    /**
+     * Sets the config data from an external source
+     * @param array $data
+     * @return mithra62\Platform\Console
+     */
+    public function setConfig(array $data)
+    {
+        $this->config = $data;
+        $this->config['db']['settings_table_name'] = $this->config['db']['prefix'].'backup_pro_settings';
+        return $this;
+    }
+    
+    /**
+     * (non-PHPdoc)
+     * @see \mithra62\Platforms\AbstractPlatform::getDbCredentials()
+     */
+    public function getDbCredentials()
+    {
+        return $this->config['db'];
+    }
+    
+    /**
+     * (non-PHPdoc)
+     * @see \mithra62\Platforms\AbstractPlatform::getEmailConfig()
+     */
+    public function getEmailConfig()
+    {
+        return $this->config['email'];
+    }
+    
+    /**
+     * (non-PHPdoc)
+     * @see \mithra62\Platforms\AbstractPlatform::getCurrentUrl()
+     */
+    public function getCurrentUrl()
+    {
+        
+    }
+    
+    /**
+     * (non-PHPdoc)
+     * @see \mithra62\Platforms\AbstractPlatform::getSiteName()
+     */
+    public function getSiteName()
+    {
+        
+    }
+    
+    /**
+     * (non-PHPdoc)
+     * @see \mithra62\Platforms\AbstractPlatform::getTimezone()
+     */
+    public function getTimezone()
+    {
+        
+    }
+    
+    /**
+     * (non-PHPdoc)
+     * @see \mithra62\Platforms\AbstractPlatform::getSiteUrl()
+     */
+    public function getSiteUrl()
+    {
+        
+    }
+    
+    /**
+     * (non-PHPdoc)
+     * @see \mithra62\Platforms\AbstractPlatform::getEncryptionKey()
+     */
+    public function getEncryptionKey()
+    {
+        
+    }
+    
+    /**
+     * (non-PHPdoc)
+     * @see \mithra62\Platforms\AbstractPlatform::getConfigOverrides()
+     */
+    public function getConfigOverrides()
+    {
+        return array();
+    }
+}
