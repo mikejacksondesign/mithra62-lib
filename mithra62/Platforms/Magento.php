5cdf0d58ad074a45db31ee9b694b4f2104ecb3a4
Eric Lamb <eric@mithra62.com>
2016-02-02 09:03:23 -0800

    Adds base Magento Platform objects

---
+++ mithra62/Platforms/Magento.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * mithra62
+ *
+ * @copyright	Copyright (c) 2015, mithra62, Eric Lamb.
+ * @link		http://mithra62.com/
+ * @version		1.0
+ * @filesource 	./mithra62/Platforms/Magento.php
+ */
+namespace mithra62\Platforms;
+
+use mithra62\Platforms\AbstractPlatform;
+
+/**
+ * mithra62 - Magento Platform Object
+ *
+ * The bridge between mithra62 code and Magento specific logic
+ *
+ * @package Platforms\Magento
+ * @author Eric Lamb <eric@mithra62.com>
+ */
+class Magento extends AbstractPlatform
+{
+    
+}
---
+++ mithra62/Platforms/View/Magento.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * mithra62 - Backup Pro
+ *
+ * @copyright	Copyright (c) 2015, mithra62, Eric Lamb.
+ * @link		http://mithra62.com/
+ * @version		3.0
+ * @filesource 	./mithra62/BackupPro/Platforms/View/Magento.php
+ */
+namespace mithra62\Platforms\View;
+
+/**
+ * Backup Pro - Magento View Object
+ *
+ * Contains the view helpers for Magento
+ *
+ * @package BackupPro\View
+ * @author Eric Lamb <eric@mithra62.com>
+ */
+class Magento extends AbstractView
+{
+}
