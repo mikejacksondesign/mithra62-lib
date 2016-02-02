e37d825ac2b5176db0c9059f1dcf5fe290086624
Eric Lamb <eric@mithra62.com>
2016-02-02 09:46:39 -0800

    Adds getCurrentUrl() logic Adds getTimezone() logic Adds getSiteUrl() logic

--- mithra62/Platforms/Magento.php
+++ mithra62/Platforms/Magento.php
@@ -21,5 +21,65 @@ use mithra62\Platforms\AbstractPlatform;
  */
 class Magento extends AbstractPlatform
 {
+    public function getDbCredentials()
+    {
+        echo \Mage::getStoreConfig('crypt/key', \Mage::app()->getStore());
+        exit;
+        $config = \Mage::getConfig();
+        print_r($config->getTablePrefix());
+        exit;
+        return array(
+            'user' => ee()->db->username,
+            'password' => ee()->db->password,
+            'database' => ee()->db->database,
+            'host' => ee()->db->hostname,
+            'prefix' => ee()->db->dbprefix,
+            'settings_table_name' => ee()->db->dbprefix . 'backup_pro_settings'
+        );        
+    }
     
+    public function getEmailConfig()
+    {
+        
+    }
+    
+    public function getCurrentUrl()
+    {
+        return \Mage::helper('core/url')->getCurrentUrl();
+    }
+    
+    public function getSiteName()
+    {
+        
+    }
+    
+    public function getTimezone()
+    {
+        return \Mage::getStoreConfig('general/locale/timezone', \Mage::app()->getStore());
+    }
+    
+    public function getSiteUrl()
+    {
+        return \Mage::getStoreConfig('web/secure/base_url', \Mage::app()->getStore());
+    }
+    
+    public function getEncryptionKey()
+    {
+        
+    }
+    
+    public function getConfigOverrides()
+    {
+        
+    }
+    
+    public function redirect($url)
+    {
+        
+    }
+    
+    public function getPost($key, $default = false)
+    {
+        
+    }
 }
