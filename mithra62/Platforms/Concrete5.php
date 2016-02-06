4722b381456f3f587ecb13d59c5c262de05457bb
Eric Lamb <eric@mithra62.com>
2016-02-05 20:27:51 -0800

    Starts Concrete5 objects

---
+++ mithra62/Platforms/Concrete5.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * mithra62
+ *
+ * @copyright	Copyright (c) 2015, mithra62, Eric Lamb.
+ * @link		http://mithra62.com/
+ * @version		1.0
+ * @filesource 	./mithra62/Platforms/Concrete5.php
+ */
+namespace mithra62\Platforms;
+
+use mithra62\Platforms\AbstractPlatform;
+use mithra62\Exceptions\PlatformsException;
+
+/**
+ * mithra62 - Concrete5 Platform Object
+ *
+ * The bridge between mithra62 code and Concrete5 specific logic
+ *
+ * @package Platforms\Concrete5
+ * @author Eric Lamb <eric@mithra62.com>
+ */
+class Concrete5 extends AbstractPlatform
+{
+    
+}
---
+++ mithra62/Platforms/View/Concrete5.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * mithra62 - Backup Pro
+ *
+ * @copyright	Copyright (c) 2015, mithra62, Eric Lamb.
+ * @link		http://mithra62.com/
+ * @version		3.0
+ * @filesource 	./mithra62/BackupPro/Platforms/View/Concrete5.php
+ */
+namespace mithra62\Platforms\View;
+
+/**
+ * Backup Pro - Concrete5 View Object
+ *
+ * Contains the view helpers for Concrete5
+ *
+ * @package BackupPro\View
+ * @author Eric Lamb <eric@mithra62.com>
+ */
+class Concrete5 extends AbstractView
+{
+}
