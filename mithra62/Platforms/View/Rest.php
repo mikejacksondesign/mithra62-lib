3b3d26f540ea74685b51830d66385cec1af8e64a
Eric Lamb <eric@mithra62.com>
2016-02-22 07:00:46 -0800

    Adds base REST view helper

---
+++ mithra62/Platforms/View/Rest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * mithra62 - Backup Pro
+ *
+ * @copyright	Copyright (c) 2015, mithra62, Eric Lamb.
+ * @link		http://mithra62.com/
+ * @version		3.0
+ * @filesource 	./mithra62/BackupPro/Platforms/View/Rest.php
+ */
+namespace mithra62\Platforms\View;
+
+/**
+ * Backup Pro - Rest View Object
+ *
+ * Contains the view helpers for Rest Requests
+ *
+ * @package BackupPro\View
+ * @author Eric Lamb <eric@mithra62.com>
+ */
+class Rest
+{
+    
+}
