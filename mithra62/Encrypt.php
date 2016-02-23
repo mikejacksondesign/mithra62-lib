87efa7a9357c80a4c0e2bd76e051a679c9baad11
Eric Lamb <eric@mithra62.com>
2016-02-22 20:18:02 -0800

    Adds guid method

--- mithra62/Encrypt.php
+++ mithra62/Encrypt.php
@@ -99,4 +99,21 @@ class Encrypt
         
         return $this->api;
     }
+    
+    /**
+     * Generates a Guid if able
+     * @return string
+     */
+    public function guid()
+    {
+        if (function_exists('com_create_guid')) {
+            return com_create_guid();
+        } else {
+            mt_srand((double) microtime() * 10000); // optional for php 4.2.0 and up.
+            $charid = strtoupper(md5(uniqid(rand(), true) . $this->getKey()));
+            $hyphen = chr(45); // "-"
+            $uuid = substr($charid, 0, 8) . $hyphen . substr($charid, 8, 4) . $hyphen . substr($charid, 12, 4) . $hyphen . substr($charid, 16, 4) . $hyphen . substr($charid, 20, 12);
+            return strtolower($uuid);
+        }
+    }    
 }
