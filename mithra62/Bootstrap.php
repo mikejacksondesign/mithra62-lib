c6322f34217964b8886fd3cd215610a5f45d6ec9
Eric Lamb <eric@mithra62.com>
2016-05-13 04:32:02 -0700

    Sets PDO to be default DB engine if available

--- mithra62/Bootstrap.php
+++ mithra62/Bootstrap.php
@@ -162,10 +162,9 @@ class Bootstrap
         $this->container['db'] = function ($c) {
             $db = new Db();
             $db->setCredentials($this->getDbConfig());
-            $type = 'pdo';
-            if( function_exists('mysqli_select_db'))
-            {
-                $type = 'mysqli';
+            $type = 'mysqli';
+            if(class_exists('Pdo')) {
+                $type = 'pdo';
             }
             
             $db->setAccessType($type);
